008b5c2bcab147f27365ab3c30487b03bb18b79d
harshita <gharshita851@gmail.com>
2026-05-06 22:15:43 +0530

    the next one.

---
+++ solution_07.py
@@ -0,0 +1,8 @@
+
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+c = int(input("enter thrid number:"))
+
+largest = max(a, b, c)
+
+print("The largest number is:", largest)
